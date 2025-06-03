f3d7ea9134fabaeb474389f8779879a1b06e5e40
TUN | Thanyanit Jongjitragan <46578644+Thanyanit-J@users.noreply.github.com>
2025-06-04 05:49:03 +0700

    feat: Add Supabase SDK and environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: false,
+  supabase: {
+    url: 'https://dgkocprznhrewuvnztbn.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRna29jcHJ6bmhyZXd1dm56dGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MzU5NzIsImV4cCI6MjA2MzUxMTk3Mn0.mp2NeBsaI8SMXJSBOiDOpUaduktew4kHnEUOkSSTNWU',
+  },
 };
