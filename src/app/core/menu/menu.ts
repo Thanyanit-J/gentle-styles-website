a52f92d2e95c07d2ede0a26711b08c7e30b7cf33
TUN | Thanyanit Jongjitragan <46578644+Thanyanit-J@users.noreply.github.com>
2025-06-06 13:11:28 +0700

    feat: List all collections to menu Well~ the menu feels so empty so I just find something to fill it 😝

--- src/app/core/menu/menu.ts
+++ src/app/core/menu/menu.ts
@@ -1,8 +1,10 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { MenuService } from '$Shared/services/menu.service';
+import { CollectionsService } from '$Shared/services/collections.service';
+import { Collection } from '$Pages/collections/collections.type';
 
 @Component({
   selector: 'app-menu',
@@ -11,9 +13,11 @@ import { MenuService } from '$Shared/services/menu.service';
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {
+export class Menu implements OnInit {
   protected readonly menuService = inject(MenuService);
+  protected readonly collectionsService = inject(CollectionsService);
   protected readonly visible = signal(false);
+  protected readonly collections = signal<Collection[]>([]);
 
   constructor() {
     this.menuService.menuOpen.subscribe(() => {
@@ -26,6 +30,12 @@ export class Menu {
 
     effect(() => {
       document.body.style.overflow = this.visible() ? 'hidden' : '';
+    });
+  }
+
+  ngOnInit(): void {
+    this.collectionsService.getAllCollections().subscribe((collections) => {
+      this.collections.set(collections);
     });
   }
 
